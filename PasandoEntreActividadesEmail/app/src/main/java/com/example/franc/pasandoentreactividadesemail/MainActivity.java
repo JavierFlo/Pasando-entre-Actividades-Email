8d3ee96eaf0bcdc39eccf67883c155db042066f1
JavierFlo <javier_caber@hotmail.com>
2016-10-12 15:45:25 -0600

    eliminando metodo setOnclickListener

--- PasandoEntreActividadesEmail/app/src/main/java/com/example/franc/pasandoentreactividadesemail/MainActivity.java
+++ PasandoEntreActividadesEmail/app/src/main/java/com/example/franc/pasandoentreactividadesemail/MainActivity.java
@@ -23,24 +23,18 @@ public class MainActivity extends AppCompatActivity {
         etMensaje = (EditText) findViewById(R.id.etMensaje);
         btnEnviar = (Button) findViewById(R.id.btnEnviar);
 
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = etEmail.getText().toString();
-                String asunto = etAsunto.getText().toString();
-                String mensaje = etMensaje.getText().toString();
+    }
+    public void EnviarMensaje (View view){
+        String email = etEmail.getText().toString();
+        String asunto = etAsunto.getText().toString();
+        String mensaje = etMensaje.getText().toString();
 
-                Intent emailenvio = new Intent(Intent.ACTION_SEND);
-                emailenvio.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
-                emailenvio.putExtra(Intent.EXTRA_SUBJECT, asunto);
-                emailenvio.putExtra(Intent.EXTRA_TEXT, mensaje);
-                emailenvio.setType("message/rfc822");
-                startActivity(Intent.createChooser(emailenvio, "Enviar correo por:"));
-
-            }
-        });
-
-
+        Intent emailenvio = new Intent(Intent.ACTION_SEND);
+        emailenvio.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+        emailenvio.putExtra(Intent.EXTRA_SUBJECT, asunto);
+        emailenvio.putExtra(Intent.EXTRA_TEXT, mensaje);
+        emailenvio.setType("message/rfc822");
+        startActivity(Intent.createChooser(emailenvio, "Enviar correo por:"));
     }
 
 
